861e0ae8a5e5dee1c372974b9f83ae2e366ff3ad
Lucas Golle <lucasgolle@pop-os.localdomain>
2022-06-19 06:36:40 -0300

    update new table

--- src/App.js
+++ src/App.js
@@ -1,64 +1,69 @@
 import "./reset.css";
 import "./App.css";
-import CartList from './Components/CartList'
-import Header from './Components/Header'
+import CartList from "./Components/CartList";
+import Header from "./Components/Header";
 import { useState, useEffect } from "react";
-import CartTotal from './Components/CartTotal'
+import CartTotal from "./Components/CartTotal";
 import ProductList from "./Components/ProductsList";
 
 function App() {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [currentSale, setCurrentSale] = useState([]);
-  
+
   useEffect(() => {
     fetch("https://hamburgueria-kenzie-json-serve.herokuapp.com/products")
       .then((response) => response.json())
       .then((data) => setProducts(data));
   }, []);
 
-  useEffect(() =>{
-    setFilteredProducts(products)
-  }, [products])
+  useEffect(() => {
+    setFilteredProducts(products);
+  }, [products]);
 
-  const showProducts = (input) =>{
-    const result = products.filter(({name, category}) => name.toLowerCase().includes(input.toLowerCase()) || category.toLowerCase().includes(input.toLowerCase()))
-    input.length > 0 ?
-    setFilteredProducts(result)
-    : setFilteredProducts(products)
-  }
+  const showProducts = (input) => {
+    const result = products.filter(
+      ({ name, category }) =>
+        name.toLowerCase().includes(input.toLowerCase()) ||
+        category.toLowerCase().includes(input.toLowerCase())
+    );
+    input.length > 0
+      ? setFilteredProducts(result)
+      : setFilteredProducts(products);
+  };
 
-  const showAllProducts = () =>{
-    setFilteredProducts(products)
-  }
+  const showAllProducts = () => {
+    setFilteredProducts(products);
+  };
 
-  const deleteAll = (list) =>{
-    const result = currentSale.filter(({id}) => id === list)
-    setCurrentSale(result)
-  }
+  const deleteAll = () => {
+    setCurrentSale([]);
+  };
 
   const handleClick = (productId) => {
-    const validation = currentSale.some(({id}) => id === productId)
-    const result = products.find(({ id }) => id === productId)
-    !validation && setCurrentSale([...currentSale, result])
+    const validation = currentSale.some(({ id }) => id === productId);
+    const result = products.find(({ id }) => id === productId);
+    !validation && setCurrentSale([...currentSale, result]);
   };
 
   const moveToTrash = (item) => {
     const result = currentSale.filter((element) => element !== item);
     setCurrentSale(result);
-  }
+  };
 
   return (
     <>
-      <Header showProducts={showProducts} showAllProducts={showAllProducts}/>
+      <Header showProducts={showProducts} showAllProducts={showAllProducts} />
       <div className="main">
-      <ProductList products={filteredProducts} handleClick={handleClick} />
-      <div>
-      <CartList currentSale={currentSale} moveToTrash={moveToTrash}/>
-      {currentSale.length > 0 && <CartTotal currentSale={currentSale} deleteAll={deleteAll}/>}
+        <ProductList products={filteredProducts} handleClick={handleClick} />
+        <div>
+          <CartList currentSale={currentSale} moveToTrash={moveToTrash} />
+          {currentSale.length > 0 && (
+            <CartTotal currentSale={currentSale} deleteAll={deleteAll} />
+          )}
+        </div>
       </div>
-      </div>
-      </>
+    </>
   );
 }
 
